534e1549c15d5d9b2219826135b5d897a8d8b472
haxAbdulAhad <abdul.ahad.21cse@bmu.edu.in>
2024-06-14 17:50:04 +0530

    added mobile screen

--- lib/mobileBody.dart
+++ lib/mobileBody.dart
@@ -5,6 +5,41 @@ class MymobileBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Placeholder();
+    return  Scaffold(
+      backgroundColor: Colors.deepPurple[200],
+      appBar: AppBar(
+        title: Text('Youtube layout - MOBILE'),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple[400],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: AspectRatio(
+
+              aspectRatio:200 / 112.5,
+              child: Container(
+                color: Colors.deepPurple[400],
+                //  height: 250,
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+                itemCount: 8,
+                itemBuilder:(context, index){
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      color: Colors.deepPurple[300],
+                      height: 120,
+                    ),
+                  );
+                }),
+          ),
+        ],
+      ),
+    );
   }
 }
